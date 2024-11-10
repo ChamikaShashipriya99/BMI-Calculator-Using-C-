5dd6f709a4cfc6e180668e1a4b8c3aabc551da4b
Chamika Shashipriya <chamikashashipriya3@gmail.com>
2024-11-10 21:45:28 +0530

    Initial commit

---
+++ BMI Calculater/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BMI_Calculater
+{
+    partial class formBMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblIndicator = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtBMI = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtIndicator = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BMI Calculator";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(24, 59);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(48, 19);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeight.Location = new System.Drawing.Point(24, 95);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(51, 19);
+            this.lblWeight.TabIndex = 2;
+            this.lblWeight.Text = "Weight";
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBMI.Location = new System.Drawing.Point(24, 129);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(38, 19);
+            this.lblBMI.TabIndex = 3;
+            this.lblBMI.Text = "BMI";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(317, 59);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(46, 19);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblIndicator
+            // 
+            this.lblIndicator.AutoSize = true;
+            this.lblIndicator.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIndicator.Location = new System.Drawing.Point(315, 95);
+            this.lblIndicator.Name = "lblIndicator";
+            this.lblIndicator.Size = new System.Drawing.Size(63, 19);
+            this.lblIndicator.TabIndex = 5;
+            this.lblIndicator.Text = "Indicator";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtHeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHeight.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(89, 56);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(215, 26);
+            this.txtHeight.TabIndex = 6;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtWeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtWeight.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWeight.Location = new System.Drawing.Point(89, 92);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(215, 26);
+            this.txtWeight.TabIndex = 7;
+            this.txtWeight.TextChanged += new System.EventHandler(this.txtWeight_TextChanged);
+            // 
+            // txtBMI
+            // 
+            this.txtBMI.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtBMI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBMI.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBMI.Location = new System.Drawing.Point(89, 126);
+            this.txtBMI.Name = "txtBMI";
+            this.txtBMI.Size = new System.Drawing.Size(215, 26);
+            this.txtBMI.TabIndex = 8;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStatus.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(381, 56);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(215, 26);
+            this.txtStatus.TabIndex = 9;
+            this.txtStatus.TextChanged += new System.EventHandler(this.txtStatus_TextChanged);
+            // 
+            // txtIndicator
+            // 
+            this.txtIndicator.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtIndicator.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIndicator.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIndicator.Location = new System.Drawing.Point(381, 92);
+            this.txtIndicator.Name = "txtIndicator";
+            this.txtIndicator.Size = new System.Drawing.Size(215, 26);
+            this.txtIndicator.TabIndex = 10;
+            this.txtIndicator.TextChanged += new System.EventHandler(this.txtIndicator_TextChanged);
+            // 
+            // btnCal
+            // 
+            this.btnCal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCal.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCal.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.Location = new System.Drawing.Point(89, 169);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(94, 31);
+            this.btnCal.TabIndex = 11;
+            this.btnCal.Text = "Calculator";
+            this.btnCal.UseVisualStyleBackColor = false;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // formBMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(606, 232);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtIndicator);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtBMI);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblIndicator);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formBMI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI Calculator";
+            this.Load += new System.EventHandler(this.formBMI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblIndicator;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtBMI;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtIndicator;
+        private System.Windows.Forms.Button btnCal;
+    }
+}
+
